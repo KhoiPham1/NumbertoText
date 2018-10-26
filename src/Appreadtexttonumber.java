b46d6db3b30d6bddb487ee75995af0771a82df53
Khoi Pham <khoi171195@gmail.com>
2018-10-26 17:33:07 +0700

    firstcommit

---
+++ src/Appreadtexttonumber.java
@@ -0,0 +1,172 @@
+import java.util.Scanner;
+
+public class Appreadtexttonumber {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter your number ");
+        int number = scn.nextInt();
+        switch (number){
+            case 0:
+                System.out.println("zero");
+                break;
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            case 6:
+                System.out.println("six");
+                break;
+            case 7:
+                System.out.println("seven");
+                break;
+            case 8:
+                System.out.println("eight");
+                break;
+            case 9:
+                System.out.println("nine");
+                break;
+            case 10:
+                System.out.println("ten");
+                break;
+        }
+        if (number>10&&number<20){
+            switch (number){
+                case 11:
+                    System.out.println("eleven");
+                    break;
+                case 12:
+                    System.out.println("twelve");
+                    break;
+                case 13:
+                    System.out.println("thirsteen");
+                    break;
+                case 14:
+                    System.out.println("fourteen");
+                    break;
+                case 15:
+                    System.out.println("fifteen");
+                    break;
+                case 16:
+                    System.out.println("sixteen");
+                    break;
+                case 17:
+                    System.out.println("seventeen");
+                    break;
+                case 18:
+                    System.out.println("eighteen");
+                    break;
+                case 19:
+                    System.out.println("nineteen");
+                    break;
+            }
+        }
+        if (number>=20&&number<100){
+            int a = number/10;
+            double b=Math.floor(a);
+            switch ((int) b){
+                case 2:
+                    System.out.print("ty ");
+                    break;
+                case 3:
+                    System.out.print("thirsty ");
+                    break;
+                case 4:
+                    System.out.print("forty ");
+                    break;
+                case 5:
+                    System.out.print("fifty ");
+                    break;
+                case 6:
+                    System.out.print("sixty ");
+                    break;
+                case 7:
+                    System.out.print("seventy ");
+                    break;
+                case 8:
+                    System.out.print("eighty ");
+                    break;
+                case 9:
+                    System.out.print("ninety ");
+                    break;
+
+            }
+            int c=number%10;
+            switch (c){
+                case 1:
+                    System.out.print("one");
+                    break;
+                case 2:
+                    System.out.print("two");
+                    break;
+                case 3:
+                    System.out.print("three");
+                    break;
+                case 4:
+                    System.out.print("four");
+                    break;
+                case 5:
+                    System.out.print("five");
+                    break;
+                case 6:
+                    System.out.print("six");
+                    break;
+                case 7:
+                    System.out.print("seven");
+                    break;
+                case 8:
+                    System.out.print("eight");
+                    break;
+                case 9:
+                    System.out.print("nine");
+                    break;
+            }
+
+        }
+        if (number>=100&&number<1000){
+            int d = number/100;
+            double e = Math.floor(d);
+            switch ((int) e){
+                case 1:
+                    System.out.print("one hundred ");
+                    break;
+                case 2:
+                    System.out.print("two hundred ");
+                    break;
+                case 3:
+                    System.out.print("three hundred ");
+                    break;
+                case 4:
+                    System.out.print("four hundred ");
+                    break;
+                case 5:
+                    System.out.print("five hundred");
+                    break;
+                case 6:
+                    System.out.print("six hundred ");
+                    break;
+                case 7:
+                    System.out.println("seven hundred");
+                    break;
+                case 8:
+                    System.out.print("eight hundred ");
+                    break;
+                case 9:
+                    System.out.print("nine hundred ");
+            }
+            
+
+        }
+
+    }
+}
